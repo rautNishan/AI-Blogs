ccb78fdc38b57f3255f786126d75f2d461e3515f
Nishan Raut <nishan.raut1709@gmail.com>
2024-06-07 21:31:46 +0545

    Removed Dist from Backend

--- back_end/dist/exceptions/DataBaseException.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DatabaseException = void 0;
-class DatabaseException {
-    constructor(message) {
-        this.message = message;
-    }
-}
-exports.DatabaseException = DatabaseException;
