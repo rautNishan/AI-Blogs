5a71f7be64ae488d4b2a05322122632c7c04ade2
Nishan Raut <nishan.raut1709@gmail.com>
2024-07-30 21:58:20 +0545

    Blog Get By Id Complete

--- front_end/src/pages/blogs/Blog.details.tsx
+++ front_end/src/pages/blogs/Blog.details.tsx
@@ -2,12 +2,27 @@ import { useParams } from "react-router-dom";
 import BlogDetailStyle from "./Blog.details.module.css";
 import { useEffect, useState } from "react";
 import { BACKEND_BASE_URL } from "../../common/constants/backend.base.url";
+import { BASE_BLOG_IMAGE_PATH } from "../../common/constants/backend-image-path.constant";
 
+export interface IPhoto {
+  associatedType: string | null;
+  associationId: string | null;
+  createdAt: string | null;
+  deletedAt: null;
+  description: null;
+  fileName: string | null;
+  id: string | null;
+  mime: string | null;
+  path: string | null;
+  size: string | null;
+  updatedAt: string | null;
+}
 export interface IBlog {
   title: string;
   subTitle: string;
   description: string;
   imageName: string | null;
+  photos: IPhoto[];
   tags: string[];
 }
 
@@ -36,12 +51,17 @@ export function BlogDetails() {
 
     fetchBlogGetById();
   }, [id, token]);
+  console.log("This is Blog: ", blog);
 
   return (
     <div className={BlogDetailStyle.mainDiv}>
-      <h1>{blog?.title}</h1>
-      <h2>{blog?.subTitle}</h2>
-      <h2>{blog?.description}</h2>
+      <div className={BlogDetailStyle.title}>{blog?.title}</div>
+      <div className={BlogDetailStyle.subTitle}>{blog?.subTitle}</div>
+      <img
+        className={BlogDetailStyle.image}
+        src={`${BASE_BLOG_IMAGE_PATH}${blog?.photos[0].fileName}`}
+      />
+      <div className={BlogDetailStyle.description}>{blog?.description}</div>
     </div>
   );
 }
